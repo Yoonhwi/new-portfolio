f78811b035bee3de6d74984669039badd60de09a
Yoonhwi <ush0105@naver.com>
2024-07-24 00:04:37 +0900

    added gsap animations to the About section triggered by scroll

---
+++ src/container/about/about-child.tsx
@@ -0,0 +1,62 @@
+import { Box, Flex, Heading } from "@chakra-ui/react";
+import { useEffect, useRef } from "react";
+import gsap from "gsap";
+import AboutText from "./about-text";
+
+interface AboutChildProps {
+  isMobile: boolean;
+}
+const AboutChild = ({ isMobile }: AboutChildProps) => {
+  const childRef = useRef<HTMLDivElement>(null);
+  const imgRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const tl = gsap.timeline({
+      defaults: { duration: 2, ease: "power3.out" },
+    });
+
+    tl.fromTo(
+      [childRef.current, imgRef.current],
+      { opacity: 0, x: (index) => ["100%", "-100%"][index] },
+      { opacity: 1, x: "0%" }
+    );
+
+    return () => {
+      tl.kill();
+    };
+  }, []);
+  return (
+    <Box color={"white"} position={"relative"} minH={"800px"} ref={childRef}>
+      {!isMobile && (
+        <Box
+          backgroundImage={`url('./img/portfolio_bg7.jpg')`}
+          backgroundSize={"cover"}
+          backgroundRepeat={"no-repeat"}
+          position={"absolute"}
+          left={0}
+          top={20}
+          bottom={0}
+          width={"650px"}
+          height={"650px"}
+          borderRadius={"100%"}
+          boxShadow={"dark-lg"}
+          ref={imgRef}
+        />
+      )}
+
+      <Flex
+        direction={"column"}
+        zIndex={1}
+        position={"relative"}
+        h={"100%"}
+        justifyContent={"center"}
+        gap={24}
+      >
+        <Heading size={"3xl"}>THE ONLY LIMIT IS MY EFFORT !</Heading>
+        <AboutText />
+      </Flex>
+    </Box>
+  );
+};
+
+export default AboutChild;
--- src/container/about/about.tsx
+++ src/container/about/about.tsx
@@ -1,55 +1,85 @@
 import { CenterLayout } from "@/components";
 import { useLayout, useSection } from "@/hooks";
-import { Box, Flex, Heading } from "@chakra-ui/react";
-import AboutText from "./about-text";
+import { Box } from "@chakra-ui/react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useEffect, useRef, useState } from "react";
+import AboutChild from "./about-child";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const About = () => {
+  const [isTrigger, setIsTrigger] = useState(false);
+
   const { registerSection } = useSection();
   const { isMobile } = useLayout();
 
+  const aboutRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const about = aboutRef.current;
+    if (!about) return;
+
+    const aboutTl = gsap.timeline({
+      scrollTrigger: {
+        trigger: about,
+        start: "top 60%",
+        onEnter: () => {
+          setIsTrigger(true);
+        },
+      },
+    });
+
+    aboutTl.fromTo(
+      about.querySelector(".overlay"),
+      {
+        clipPath: "inset(0 0 100% 0)",
+      },
+      {
+        clipPath: "inset(0 0 0% 0)",
+        duration: 2,
+        ease: "power2.inOut",
+      }
+    );
+
+    return () => {
+      aboutTl.kill();
+    };
+  }, [isMobile, setIsTrigger]);
+
   return (
     <Box
-      backgroundColor={"rgb(27,29,32)"}
-      backgroundImage={isMobile ? `url('./img/portfolio_bg7.jpg')` : ""}
-      backgroundSize={"cover"}
-      backgroundRepeat={"no-repeat"}
+      position="relative"
       minH={`100vh`}
       ref={(el) => {
         if (!el) return;
         registerSection("ABOUT", el);
+        aboutRef.current = el;
       }}
       py={20}
       px={12}
+      backgroundImage="url('./img/portfolio_bg.jpg')"
+      backgroundSize="cover"
+      backgroundRepeat="no-repeat"
     >
+      <Box
+        className="overlay"
+        position="absolute"
+        top={0}
+        left={0}
+        right={0}
+        bottom={0}
+        backgroundColor="rgb(27,29,32)"
+        backgroundImage={
+          isTrigger && isMobile ? `url('./img/portfolio_bg7.jpg')` : "none"
+        }
+        backgroundSize="cover"
+        backgroundRepeat="no-repeat"
+        clipPath="inset(0 0 100% 0)"
+      />
+
       <CenterLayout>
-        <Box color={"white"} position={"relative"} minH={"800px"}>
-          {!isMobile && (
-            <Box
-              backgroundImage={`url('./img/portfolio_bg7.jpg')`}
-              backgroundSize={"cover"}
-              backgroundRepeat={"no-repeat"}
-              position={"absolute"}
-              left={0}
-              top={20}
-              bottom={0}
-              width={"650px"}
-              height={"650px"}
-              borderRadius={"100%"}
-              boxShadow={"dark-lg"}
-            />
-          )}
-          <Flex
-            direction={"column"}
-            zIndex={1}
-            position={"relative"}
-            h={"100%"}
-            justifyContent={"center"}
-            gap={24}
-          >
-            <Heading size={"3xl"}>THE ONLY LIMIT IS MY EFFORT !</Heading>
-            <AboutText />
-          </Flex>
-        </Box>
+        {isTrigger && <AboutChild isMobile={isMobile} />}
       </CenterLayout>
     </Box>
   );
